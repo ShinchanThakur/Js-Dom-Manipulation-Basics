e6933e84cc9f5dd7009fd90aed557b8b4f46be87
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-07 19:35:17 +0530

    Text content vs Inner text

--- selectors.js
+++ selectors.js
@@ -1,2 +1,3 @@
 var headerTitle = document.getElementById("header-title");
-console.log(headerTitle);
+headerTitle.textContent = 'Hello';  //shows everything present between starting and ending tags
+headerTitle.innerText = 'Goodbye';  //shows what the user will see on final screen
