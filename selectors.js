f6099b8e74306b685d0bd32e29ac6009f712eea3
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-08 16:36:06 +0530

    Styling selected element

--- selectors.js
+++ selectors.js
@@ -2,3 +2,6 @@ var headerTitle = document.getElementById("header-title");
 headerTitle.textContent = 'Hello';  //shows all text present between starting and ending tags
 headerTitle.innerText = 'Goodbye';  //shows what the user will see on final screen
 headerTitle.innerHTML = '<h3>Hello</h3>';   //shows all HTML content between starting and ending tags
+
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
