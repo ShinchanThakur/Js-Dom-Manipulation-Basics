e90473d26dcdb796f5aafe5f861444e36d4416a1
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-08 11:39:58 +0530

    Inner HTML

--- selectors.js
+++ selectors.js
@@ -1,3 +1,4 @@
 var headerTitle = document.getElementById("header-title");
-headerTitle.textContent = 'Hello';  //shows everything present between starting and ending tags
+headerTitle.textContent = 'Hello';  //shows all text present between starting and ending tags
 headerTitle.innerText = 'Goodbye';  //shows what the user will see on final screen
+headerTitle.innerHTML = '<h3>Hello</h3>';   //shows all HTML content between starting and ending tags
