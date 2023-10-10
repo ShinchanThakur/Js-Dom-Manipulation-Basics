3e5150decfb43a8c91515c7d7434a9f42076a1d8
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-10 19:58:56 +0530

    parentElement

--- domTraversal.js
+++ domTraversal.js
@@ -3,3 +3,9 @@ var itemList = document.querySelector('#items');
 console.log(itemList.parentNode);
 itemList.parentNode.style.backgroundColor = '#f4f4f4';
 console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+var itemList = document.querySelector('#items');
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
