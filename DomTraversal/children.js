cd5e8e8aea499f9ada4c4650e36758fb4e91c8d9
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-10 20:21:03 +0530

    firstChild

--- DomTraversal/children.js
+++ DomTraversal/children.js
@@ -8,3 +8,9 @@ console.log(itemList.childNodes);
 console.log(itemList.children);
 console.log(itemList.children[1]);
 itemList.children[1].style.backgroundColor = 'yellow';
+
+// firstChild
+console.log(itemList.firstChild);
+// Issue -> It also considers newLine etc as child element
+// Therefore it is not preferable
+
