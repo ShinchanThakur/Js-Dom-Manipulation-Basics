5d960d08ec0535cfb01dc6e89f8e4f28769acd98
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-10 20:16:29 +0530

    children

--- DomTraversal/children.js
+++ DomTraversal/children.js
@@ -4,3 +4,7 @@ var itemList = document.querySelector('#items');
 console.log(itemList.childNodes);
 // Issue -> It also considers newLine etc as child element
 // Therefore it is not preferable
+
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
