221158463e9819bc79e015c399ff320b036e5bb6
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-11 10:12:19 +0530

    Event listener using named function

--- Events/NewWay/action.js
+++ Events/NewWay/action.js
@@ -1,4 +1,13 @@
+// anonymous function
 document.getElementById('button')
 .addEventListener('click', function() {
     console.log(123);
 });
+
+// named function
+document.getElementById('button2')
+.addEventListener('click', buttonClick);
+
+function buttonClick() {
+    console.log('Button Clicked')
+}
