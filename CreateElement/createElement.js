4b7bcc2790f401b15c772ae81b6f9b2ce320afaf
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-11 08:47:54 +0530

    Adding new element to DOM

--- CreateElement/createElement.js
+++ CreateElement/createElement.js
@@ -4,4 +4,9 @@ newDiv.id = 'myId';
 newDiv.setAttribute('title', 'Hello Div');
 var newDivText = document.createTextNode('Hello World');
 newDiv.appendChild(newDivText);
+newDiv.style.fontSize = '30px';
 console.log(newDiv);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1);
