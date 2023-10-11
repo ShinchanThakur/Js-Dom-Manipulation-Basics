ac1f138b528a1a5a0ae8b193074a1e99291fb5c8
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-11 20:48:32 +0530

    Changing color on mouse movement

--- Events/NewWay/someExperiments.js
+++ Events/NewWay/someExperiments.js
@@ -6,4 +6,7 @@ var output = document.getElementById('output');
 function runEvent(e) {
     console.log('EVENT TYPE: ' + e.type);
     output.innerHTML = '<h3>MouseX: ' + e.offsetY + '</hr3><h3>MouseY: ' + e.offsetY + '</h3>';
+
+    box.style.backgroundColor = 'rgb(' + e.offsetX + ',' + e.offsetY + ',40)';
+    document.body.style.backgroundColor = 'rgb(' + e.offsetX + ',' + e.offsetY + ',40)';
 }
