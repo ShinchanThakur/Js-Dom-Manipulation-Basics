a59a2b13d333a8a4b6c2c429f92e92b1fe0fb4aa
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-11 17:23:49 +0530

    Printing position of click

--- Events/NewWay/eventObject.js
+++ Events/NewWay/eventObject.js
@@ -6,4 +6,13 @@ function buttonClick(e) {
     console.log(e.target.id);
     console.log(e.target.className);
     console.log(e.target.classList);
+
+    var output = document.getElementById('output');
+    output.innerHTML = '<h3>' + e.target.id + '<h3>';
+
+    console.log(e.type);
+    console.log(e.clientX);     // Position of click wrt Window
+    console.log(e.clientY);
+    console.log(e.offsetX);     // Position of click wrt the clicked element
+    console.log(e.offsetY);
 }
