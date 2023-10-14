0dbbf7644c0a5e0e70a37b231f890b073b7480a6
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-14 17:30:46 +0530

    Added event listener to form

---
+++ FinalProject/main.js
@@ -0,0 +1,9 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+form.addEventListener('submit', addItem);
+
+function addItem(e) {
+    e.preventDefault();
+
+}
